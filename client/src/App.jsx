d8cf22db2deb8c73aa46bde282d3df2d27ab5c4f
srt1017 <omarfarouq107@gmail.com>
2023-07-24 18:10:07 -0500

    set up boilerplate

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import Canvas from './canvas'
+import Customiser from './pages/Customiser'
+import Home from './pages/Home'
+
+function App() {
+  
+  return (
+  <main className='app transition-all-ease-in'>
+    <Home />
+    <Canvas />
+    <Customiser />
+  </main>
+  )
+}
+
+export default App
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import { motion, AnimatePresence } from "framer-motion"
+import { useSnapshot } from "valtio"
+import { headContainerAnimation, headtContentAnimation, headTextAnimation, slideAnimation } from '../config/motion'
+
+const Home = () => {
+  return (
+    <div>Home</div>
+  )
+}
+
+export default Home

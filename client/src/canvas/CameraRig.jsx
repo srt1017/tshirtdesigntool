5c38d0139de286d066ebb0ec4a87db5535beb9ee
srt1017 <omarfarouq107@gmail.com>
2023-07-25 13:44:42 -0500

    made initial layout. working on t shirt model

---
+++ client/src/canvas/CameraRig.jsx
@@ -0,0 +1,30 @@
+import React, { useRef } from 'react'
+import { useFrame } from '@react-three/fiber'
+import { easing } from 'maath'
+import { useSnapshot } from 'valtio'
+
+import state from '../store'
+
+const CameraRig = ({ children }) => {
+  const group = useRef()
+  const snap = useSnapshot(state)
+
+  //set model rotation
+
+  useFrame((state, delta) => {
+    easing.dampE(
+      group.current.rotation,
+      [state.pointer.y / 10, -state.pointer.x / 5, 0],
+      0.25,
+      delta
+    )
+  })
+
+  return (
+    <group ref={group}>
+      {children}
+    </group>
+  )
+}
+
+export default CameraRig
